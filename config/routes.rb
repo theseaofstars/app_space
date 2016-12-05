61dc857149d57c5779e3938cd581536657901422
theseaofstars <theseaofstars@live.cn>
2016-12-05 22:36:45 +0800

    add signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
 
+
+
   get '/about', to: 'static_pages#about'
 
   get '/help',to: 'static_pages#help'
 
   get '/contact',to: 'static_pages#contact'
 
+  get '/signup',to: 'users#new'
+
   root 'static_pages#home'
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
