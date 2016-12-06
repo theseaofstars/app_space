2b3acba318ce190978804bccbbc991569d8dea1f
theseaofstars <theseaofstars@live.cn>
2016-12-06 19:42:51 +0800

    write some test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,29 +1,53 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
-    assert_response :success
-  end
 
-  test "should get create" do
-    get users_create_url
+
+  test "should get new" do
+    get signup_path
     assert_response :success
   end
 
   test "should get edit" do
-    get users_edit_url
+    get edit_user_path
     assert_response :success
   end
 
-  test "should get update" do
-    get users_update_url
+  test "should get index" do
+    get users_path
     assert_response :success
   end
 
-  test "should get destroy" do
-    get users_destroy_url
+  test "should get show" do
+    get user_path(users(:mecheal))
     assert_response :success
   end
-
+/#
+  test "should delete a user" do
+    user = users(:mecheal)
+    assert_difference "User.count",-1 do
+      delete users_path,params: {
+          user: {
+              name: user.name,
+              email: user.email,
+              password: user.password,
+              password_confirmation: user.password_confirmation
+          }
+      }
+    end
+  end
+#/
+/#  test "should create a user" do
+    assert_difference "User.count",1 do
+      post users_path,params: {
+          user: {
+              name: "test",
+              email: "test@live.cn",
+              password: "foobar",
+              password_confirmation: "foobar"
+          }
+      }
+    end
+  end
+#/
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,37 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "wusheng",email: "th@live.cn",
+    password: "123456",password_confirmation: "123456")
+  end
+
+  test "user should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should not be empty" do
+    @user.name = "  "
+    assert_not @user.valid?
+  end
+
+  test "email should not be empty" do
+    @user.email = "  "
+    assert_not @user.valid?
+  end
+
+  test "user name should shorter than 50" do
+    @user.name = "a"*51
+    assert_not @user.valid?
+  end
+
+  test "user email should shorter than 254" do
+    @user.email = "a"*255
+    assert_not @user.valid?
+  end
+
+  test "user email should be unique" do
+    @user.save
+    assert_not @user.valid?
+  end
 end
