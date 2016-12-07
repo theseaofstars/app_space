50d87da2ca74e2466b01ae5ea9878e81bae9fb27
theseaofstars <theseaofstars@live.cn>
2016-12-07 23:14:10 +0800

    add new user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find_by(id:params[:id])
   end
 
   def new
@@ -11,6 +13,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      redirect_to user_path(@user)
+    else
+      render 'users/new'
+    end
+
   end
 
   def edit
@@ -21,4 +31,8 @@ class UsersController < ApplicationController
 
   def destroy
   end
+  private
+    def user_params
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
 end
